dd53fb4a4c4d3dfdf79fd518e311a3f40d1ac4c9
최영환 <117884300+choiyounghwan123@users.noreply.github.com>
2024-11-10 17:19:20 +0900

    Time: 0 ms (100%), Space: 16.7 MB (22.09%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.py
@@ -0,0 +1,24 @@
+# 75. Sort Colors
+
+class Solution:
+    def sortColors(self, nums: list[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        red,white,blue = 0,0,len(nums)
+
+        while white < blue:
+            print(red,white,blue)
+            if nums[white] < 1:
+                nums[red],nums[white] = nums[white],nums[red]
+                red += 1
+                white += 1
+            elif nums[white] > 1:
+                blue -= 1
+                nums[blue],nums[white] = nums[white],nums[blue]
+            else:
+                white +=1
+        print(nums)
+
+
+print(Solution().sortColors(nums = [2,0,2,1,1,0]))
