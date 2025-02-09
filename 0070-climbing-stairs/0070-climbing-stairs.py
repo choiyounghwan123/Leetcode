e436b9edf03c4e2de15195f5e329a34b470b9322
최영환 <117884300+choiyounghwan123@users.noreply.github.com>
2025-02-09 14:08:45 +0900

    Time: 0 ms (100%), Space: 17.6 MB (86.23%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,16 +1,10 @@
-# 70.Climbing Stairs
+# 70. Climbing Stairs
 
 class Solution:
     def climbStairs(self, n: int) -> int:
-        dp = [0] * (n+1)
-
+        dp = [0] * 46
         dp[1] = 1
-        if n == 2 or n == 1:
-            return 2 if n == 2 else 1
         dp[2] = 2
-
-        for i in range(3, n+1):
-            dp[i] = dp[i-1] + dp[i-2]
+        for i in range(3, n + 1):
+            dp[i] = dp[i-2] + dp[i-1]
         return dp[n]
-
-print(Solution().climbStairs(2))
