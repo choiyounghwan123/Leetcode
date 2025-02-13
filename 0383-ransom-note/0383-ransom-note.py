b5149dc283a2a14ee498e05cd12d8675d60c49ca
최영환 <117884300+choiyounghwan123@users.noreply.github.com>
2025-02-14 00:23:54 +0900

    Time: 27 ms (28.21%), Space: 17.7 MB (92.57%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -3,18 +3,17 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         hash_map = dict()
-        for char in ransomNote:
+        for char in magazine:
             hash_map[char] = hash_map.get(char,0) + 1
 
-        for char in magazine:
-            if char not in hash_map:
-                continue
-            hash_map[char]-=1
-
-        for key,value in hash_map.items():
-            if value > 0:
+        for c in ransomNote:
+            if c not in hash_map or hash_map[c] <= 0:
                 return False
+            hash_map[c] -= 1
         return True
+
+
+
 
 solution = Solution()
 print(solution.canConstruct(ransomNote = "aa", magazine = "aab"))
