f0bf41e21b1825c74fd6832977bd5418059da9ec
최영환 <117884300+choiyounghwan123@users.noreply.github.com>
2025-02-15 16:08:03 +0900

    Time: 2 ms (49.16%), Space: 17.8 MB (77.9%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.py
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.py
@@ -2,17 +2,9 @@
 
 class Solution:
     def checkIfExist(self, arr: list[int]) -> bool:
-        hash_map = dict()
-
         for i in range(len(arr)):
-            if arr[i] * 2 in hash_map and hash_map[arr[i] * 2] == 0:
-                print(arr[i])
-                print("1")
+            if arr[i] * 2 in arr and arr.index(arr[i] * 2) != i:
                 return True
-            elif arr[i] in hash_map and hash_map[arr[i]] == 1:
-                return True
-            hash_map[arr[i]] = 0
-            hash_map[arr[i] * 2] = 1
         return False
 
 
