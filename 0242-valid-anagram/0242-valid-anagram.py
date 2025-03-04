7c481b2bf8c42e053ff88c33df445b2075ead78d
최영환 <117884300+choiyounghwan123@users.noreply.github.com>
2025-03-04 13:57:42 +0900

    Time: 11 ms (72.87%), Space: 17.7 MB (86.34%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -2,6 +2,19 @@
 
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s) == sorted(t)
+        if len(s) != len(t):
+            return False
+        hash_map = dict()
 
-print(Solution().isAnagram(s = "anagram", t = "nagaram"))
+        for c in s:
+            hash_map[c] = hash_map.get(c,0) + 1
+
+        for c in t:
+            if c in hash_map and hash_map[c] > 0:
+                hash_map[c] -= 1
+            else:
+                return False
+        return True
+
+solution = Solution()
+print(solution.isAnagram( s = "anagram", t = "nagaram"))
