33acf12c2400b577141327d627f83a54ae492ea5
최영환 <117884300+choiyounghwan123@users.noreply.github.com>
2025-03-04 09:24:26 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -0,0 +1,17 @@
+# 7. Reverse Integer
+
+class Solution:
+    def reverse(self, x: int) -> int:
+        x = list(str(x))
+        x.reverse()
+        x = "".join(x)
+        if x[-1] == "-":
+            x = -(int(x[:-1]))
+        else:
+            x = int(x)
+        if -2147483648 >= x and x >= 2147483648:
+            return 0
+        else:
+            return x
+solution = Solution()
+print(solution.reverse(x = -123))
