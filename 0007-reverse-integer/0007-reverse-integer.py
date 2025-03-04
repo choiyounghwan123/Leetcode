fe5e724aaa2b05793619fce9d480873e5be60712
최영환 <117884300+choiyounghwan123@users.noreply.github.com>
2025-03-04 09:31:21 +0900

    Time: 38 ms (65.66%), Space: 17.9 MB (16.71%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -9,9 +9,9 @@ class Solution:
             x = -(int(x[:-1]))
         else:
             x = int(x)
-        if -2147483648 >= x and x >= 2147483648:
+        if -2147483648 >= x or x >= 2147483648:
             return 0
         else:
             return x
 solution = Solution()
-print(solution.reverse(x = -123))
+print(solution.reverse(x = 1534236469))
