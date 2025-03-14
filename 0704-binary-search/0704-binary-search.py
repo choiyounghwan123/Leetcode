ac47d7540a60019188a16f25fe4f167a84a8bf48
최영환 <117884300+choiyounghwan123@users.noreply.github.com>
2025-03-14 20:40:20 +0900

    Time: 0 ms (100%), Space: 26.1 MB (6.31%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -2,18 +2,19 @@
 
 class Solution:
     def search(self, nums: list[int], target: int) -> int:
-        left, right = 0, len(nums) - 1
+        def binary_search(start,end):
+            if start > end:
+                return -1
 
-        while left <= right:
-            mid = (left + right) // 2
-            
-            if nums[mid] > target:
-                right = mid -1
-            elif nums[mid] < target:
-                left = mid+1
-            else:
+            mid = (start + end) // 2
+
+            if nums[mid] == target:
                 return mid
-        return -1
+            elif nums[mid] > target:
+                return binary_search(start,mid-1)
+            else:
+                return binary_search(mid+1,end)
+        return binary_search(0,len(nums) - 1)
 
-
-print(Solution().search(nums=[-1, 0, 3, 5, 9, 12], target=9))
+solution = Solution()
+print(solution.search(nums = [-1,0,3,5,9,12], target = 9))
